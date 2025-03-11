95105a376f4864b4de67d8ed2d094fa61aa22dec
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-11 13:18:21 -0400

    new TODO's all over the place including new packing with empty classes

--- src/objects/Cannon.java
+++ src/objects/Cannon.java
@@ -1,29 +1,20 @@
 package objects;
 
-public class Cannon extends GameObject{
+public class Cannon extends GameObject {
 
     public Cannon(int x, int y, int objType) {
         // TODO: call super passing in x, y, objType
-        // TODO: call createHitbox()
-    }
-
-    private void createHitbox(){
-        // TODO: if else statement here
-        // check if objType is a box
-        // TODO: if so call initHitbox(25, 18) then
-        // TODO: set xDrawOffset to (int) (7 * Game.SCALE)
-        // TODO: set yDrawOffset to (int) (12 * Game.SCALE)
-        // else
-        // TODO: call initHitbox(23, 25) then
-        // TODO: set xDrawOffset to (int) (8 * Game.SCALE)
-        // TODO: set yDrawOffset to (int) (5 * Game.SCALE)
-        // end of if else block
-        // TODO: add yDrawOffset + (int) (Game.SCALE * 2) to hitbox.y
-        // TODO: add xDrawOffset / 2 to hitbox.x
+        // TODO: set tileY to y / Game.TILES_SIZE
+        // TODO: call initHitbox() passing in 40, 26
+        // TODO: subtract (int)(4 * Game.SCALE) from hitbox.x
+        // TODO: add (int)(6 * Game.SCALE) to hitbox.y
     }
 
     public void update(){
         // TODO: check if doAnimation is true and if so updateAnimationTick()
     }
 
+    public int getTileY() {
+        // TODO: return tileY
+    }
 }
