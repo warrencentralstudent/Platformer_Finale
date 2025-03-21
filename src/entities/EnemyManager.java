1d900535dd95d32af97b8be6608d02255c511d98
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-21 05:41:17 -0400

    Enemy and Entity

--- src/entities/EnemyManager.java
+++ src/entities/EnemyManager.java
@@ -18,41 +18,83 @@ public class EnemyManager {
 	private ArrayList<Crabby> crabbies = new ArrayList<>();
 
 	public EnemyManager(Playing playing) {
-		// TODO: coming soon
+		// TODO: set this.playing to playing
+		// TODO: call loadEnemyImgs()
 
 	}
 
 	public void loadEnemies(Level level) {
-		// TODO: coming soon
+		// TODO: set crabbies to level.getCrabs()
 	}
 
 	public void update(int[][] lvlData, Player player) {
-		// TODO: coming soon
-
+		// TODO: make a boolean named isAnyActive and assign false to it
+		// TODO: for loop Crabby c : crabbies
+		// for loop begin
+		// TODO: check if c.isActive()
+		// if begin
+		// TODO: call c.update() passing in lvlData and player
+		// TODO: assign true to isAnyActive
+		// if end
+		// for end
+		// TODO: check if !isAnyActive
+		// if begin
+		// TODO: call playing.setLevelCompleted passing in true
+		// if end
 	}
 
 	public void draw(Graphics g, int xLvlOffset) {
-		// TODO: coming soon
+		// TODO: call drawCrabs() passing in g and xLvlOffset
 	}
 
 	private void drawCrabs(Graphics g, int xLvlOffset) {
-		// TODO: coming soon
+		// TODO: freebie
+		for (Crabby c : crabbies)
+			if (c.isActive()) {
+
+				g.drawImage(crabbyArr[c.getState()][c.getAniIndex()], (int) c.getHitbox().x - xLvlOffset - CRABBY_DRAWOFFSET_X + c.flipX(), (int) c.getHitbox().y - CRABBY_DRAWOFFSET_Y,
+						CRABBY_WIDTH * c.flipW(), CRABBY_HEIGHT, null);
+
+//				c.drawHitbox(g, xLvlOffset);
+//				c.drawAttackBox(g, xLvlOffset);
+			}
 
 	}
 
 	public void checkEnemyHit(Rectangle2D.Float attackBox) {
-		// TODO: coming soon
-
+		// TODO: for loop Crabby c : crabbies
+		// for loop begin
+		// TODO: check if c.isActive()
+		// if begin
+		// TODO: check if c.getCurrentHealth is greater than 0
+		// if begin
+		// TODO: check if attackBox.intersects(c.getHitbox())
+		// if begin
+		// TODO: call c.hurt() passing in 10
+		// TODO: return
+		// if end
+		// if end
+		// if end
+		// for end
 	}
 
 	private void loadEnemyImgs() {
-		// TODO: coming soon
-
+		// TODO: assign a new BufferedImage[5][9] to crabbyArr
+		// TODO: create a BufferedImage called temp and assign LoadSave.GetSpriteAtlas() passing in LoadSave.CRABBY_SPRITE
+		// TODO: for loop int j starting at 0, less than crabbyAr.length, add 1 each iteration
+		// for loop begin
+		// TODO: for loop int i starting at 0, less than crabby[j].length, add 1 each iteration
+		// for loop begin
+		// TODO: assign to crabbyArr[j][i] the value of temp.getSubimage(i * CRABBY_WIDTH_DEFAULT, j * CRABBY_HEIGHT_DEFAULT, CRABBY_WITH_DEFAULT, CRABBY_HEIGHT_DEFAULT)
+		// for loop end
+		// for loop end
 	}
 
 	public void resetAllEnemies() {
-		// TODO: coming soon
-
+		// TODO: for loop Crabby c : crabbies
+		// for loop begin
+		// TODO: call c.resetEnemy()
+		//for loop end
 	}
 
 }
