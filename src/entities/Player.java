5641bb35002e77e81bc0c18f0163a6b7f56d5477
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-21 05:47:58 -0400

    Entities all done.

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,19 +1,18 @@
 package entities;
 
-import audio.AudioPlayer;
-import gamestates.Playing;
-import utilz.Constants;
-import utilz.LoadSave;
+import static utilz.Constants.PlayerConstants.*;
+import static utilz.HelpMethods.*;
+import static utilz.Constants.*;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
-import static utilz.Constants.*;
-import static utilz.Constants.ANI_SPEED;
-import static utilz.Constants.GRAVITY;
-import static utilz.Constants.PlayerConstants.*;
-import static utilz.HelpMethods.*;
+import audio.AudioPlayer;
+import gamestates.Playing;
+import utilz.LoadSave;
 
 public class Player extends Entity {
 
@@ -63,173 +62,385 @@ public class Player extends Entity {
 	private int powerGrowSpeed = 15;
 	private int powerGrowTick;
 
+	// TODO: one more freebie
+
 	public Player(float x, float y, int width, int height, Playing playing) {
 		super(x, y, width, height);
-		// TODO: coming soon
-
+		this.playing = playing;
+		this.state = IDLE;
+		this.maxHealth = 100;
+		this.currentHealth = 35;
+		this.walkSpeed = Game.SCALE * 1.0f;
+		loadAnimations();
+		initHitbox(20, 27);
+		initAttackBox();
 	}
 
 	public void setSpawn(Point spawn) {
-		// TODO: coming soon
-
+		this.x = spawn.x;
+		this.y = spawn.y;
+		hitbox.x = x;
+		hitbox.y = y;
 	}
 
 	private void initAttackBox() {
-		// TODO: coming soon
-
+		attackBox = new Rectangle2D.Float(x, y, (int) (20 * Game.SCALE), (int) (20 * Game.SCALE));
+		resetAttackBox();
 	}
 
 	public void update() {
-		// TODO: coming soon
+		updateHealthBar();
+		updatePowerBar();
 
+		if (currentHealth <= 0) {
+			if (state != DEAD) {
+				state = DEAD;
+				aniTick = 0;
+				aniIndex = 0;
+				playing.setPlayerDying(true);
+				playing.getGame().getAudioPlayer().playEffect(AudioPlayer.DIE);
+			} else if (aniIndex == GetSpriteAmount(DEAD) - 1 && aniTick >= ANI_SPEED - 1) {
+				playing.setGameOver(true);
+				playing.getGame().getAudioPlayer().stopSong();
+				playing.getGame().getAudioPlayer().playEffect(AudioPlayer.GAMEOVER);
+			} else
+				updateAnimationTick();
+
+			return;
+		}
+
+		updateAttackBox();
+
+		updatePos();
+		if (moving) {
+			checkPotionTouched();
+			checkSpikesTouched();
+			tileY = (int) (hitbox.y / Game.TILES_SIZE);
+			if (powerAttackActive) {
+				powerAttackTick++;
+				if (powerAttackTick >= 35) {
+					powerAttackTick = 0;
+					powerAttackActive = false;
+				}
+			}
+		}
+		if (attacking || powerAttackActive)
+			checkAttack();
+
+		updateAnimationTick();
+		setAnimation();
 	}
 
 	private void checkSpikesTouched() {
-		// TODO: coming soon
+		playing.checkSpikesTouched(this);
 
 	}
 
 	private void checkPotionTouched() {
-		// TODO: coming soon
+		playing.checkPotionTouched(hitbox);
 	}
 
 	private void checkAttack() {
-		// TODO: coming soon
+		if (attackChecked || aniIndex != 1)
+			return;
+		attackChecked = true;
 
+		if (powerAttackActive)
+			attackChecked = false;
+
+		playing.checkEnemyHit(attackBox);
+		playing.checkObjectHit(attackBox);
+		playing.getGame().getAudioPlayer().playAttackSound();
 	}
 
 	private void updateAttackBox() {
-		// TODO: coming soon
+		if (right && left) {
+			if (flipW == 1) {
+				attackBox.x = hitbox.x + hitbox.width + (int) (Game.SCALE * 10);
+			} else {
+				attackBox.x = hitbox.x - hitbox.width - (int) (Game.SCALE * 10);
+			}
 
+		} else if (right || (powerAttackActive && flipW == 1))
+			attackBox.x = hitbox.x + hitbox.width + (int) (Game.SCALE * 10);
+		else if (left || (powerAttackActive && flipW == -1))
+			attackBox.x = hitbox.x - hitbox.width - (int) (Game.SCALE * 10);
+
+		attackBox.y = hitbox.y + (Game.SCALE * 10);
 	}
 
 	private void updateHealthBar() {
-		// TODO: coming soon
+		healthWidth = (int) ((currentHealth / (float) maxHealth) * healthBarWidth);
 	}
 
 	private void updatePowerBar() {
-		// TODO: coming soon
+		powerWidth = (int) ((powerValue / (float) powerMaxValue) * powerBarWidth);
 
+		powerGrowTick++;
+		if (powerGrowTick >= powerGrowSpeed) {
+			powerGrowTick = 0;
+			changePower(1);
+		}
 	}
 
 	public void render(Graphics g, int lvlOffset) {
-		// TODO: coming soon
-
+		g.drawImage(animations[state][aniIndex], (int) (hitbox.x - xDrawOffset) - lvlOffset + flipX, (int) (hitbox.y - yDrawOffset), width * flipW, height, null);
+//		drawHitbox(g, lvlOffset);
+//		drawAttackBox(g, lvlOffset);
+		drawUI(g);
 	}
 
 	private void drawUI(Graphics g) {
-		// TODO: coming soon
+		// Background ui
+		g.drawImage(statusBarImg, statusBarX, statusBarY, statusBarWidth, statusBarHeight, null);
 
+		// Health bar
+		g.setColor(Color.red);
+		g.fillRect(healthBarXStart + statusBarX, healthBarYStart + statusBarY, healthWidth, healthBarHeight);
+
+		// Power Bar
+		g.setColor(Color.yellow);
+		g.fillRect(powerBarXStart + statusBarX, powerBarYStart + statusBarY, powerWidth, powerBarHeight);
 	}
 
 	private void updateAnimationTick() {
-		// TODO: coming soon
-
+		aniTick++;
+		if (aniTick >= ANI_SPEED) {
+			aniTick = 0;
+			aniIndex++;
+			if (aniIndex >= GetSpriteAmount(state)) {
+				aniIndex = 0;
+				attacking = false;
+				attackChecked = false;
+			}
+		}
 	}
 
 	private void setAnimation() {
-		// TODO: coming soon
+		int startAni = state;
 
+		if (moving)
+			state = RUNNING;
+		else
+			state = IDLE;
+
+		if (inAir) {
+			if (airSpeed < 0)
+				state = JUMP;
+			else
+				state = FALLING;
+		}
+
+		if (powerAttackActive) {
+			state = ATTACK;
+			aniIndex = 1;
+			aniTick = 0;
+			return;
+		}
+
+		if (attacking) {
+			state = ATTACK;
+			if (startAni != ATTACK) {
+				aniIndex = 1;
+				aniTick = 0;
+				return;
+			}
+		}
+		if (startAni != state)
+			resetAniTick();
 	}
 
 	private void resetAniTick() {
-		// TODO: coming soon
-
+		aniTick = 0;
+		aniIndex = 0;
 	}
 
 	private void updatePos() {
-		// TODO: coming soon
+		moving = false;
 
+		if (jump)
+			jump();
+
+		if (!inAir)
+			if (!powerAttackActive)
+				if ((!left && !right) || (right && left))
+					return;
+
+		float xSpeed = 0;
+
+		if (left && !right) {
+			xSpeed -= walkSpeed;
+			flipX = width;
+			flipW = -1;
+		}
+		if (right && !left) {
+			xSpeed += walkSpeed;
+			flipX = 0;
+			flipW = 1;
+		}
+
+		if (powerAttackActive) {
+			if ((!left && !right) || (left && right)) {
+				if (flipW == -1)
+					xSpeed = -walkSpeed;
+				else
+					xSpeed = walkSpeed;
+			}
+
+			xSpeed *= 3;
+		}
+
+		if (!inAir)
+			if (!IsEntityOnFloor(hitbox, lvlData))
+				inAir = true;
+
+		if (inAir && !powerAttackActive) {
+			if (CanMoveHere(hitbox.x, hitbox.y + airSpeed, hitbox.width, hitbox.height, lvlData)) {
+				hitbox.y += airSpeed;
+				airSpeed += GRAVITY;
+				updateXPos(xSpeed);
+			} else {
+				hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, airSpeed);
+				if (airSpeed > 0)
+					resetInAir();
+				else
+					airSpeed = fallSpeedAfterCollision;
+				updateXPos(xSpeed);
+			}
+
+		} else
+			updateXPos(xSpeed);
+		moving = true;
 	}
 
 	private void jump() {
-		// TODO: coming soon
-
+		if (inAir)
+			return;
+		playing.getGame().getAudioPlayer().playEffect(AudioPlayer.JUMP);
+		inAir = true;
+		airSpeed = jumpSpeed;
 	}
 
 	private void resetInAir() {
-		// TODO: coming soon
-
+		inAir = false;
+		airSpeed = 0;
 	}
 
 	private void updateXPos(float xSpeed) {
-		// TODO: coming soon
+		if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData))
+			hitbox.x += xSpeed;
+		else {
+			hitbox.x = GetEntityXPosNextToWall(hitbox, xSpeed);
+			if (powerAttackActive) {
+				powerAttackActive = false;
+				powerAttackTick = 0;
+			}
 
+		}
 	}
 
 	public void changeHealth(int value) {
-		// TODO: coming soon
+		currentHealth += value;
 
+		if (currentHealth <= 0)
+			currentHealth = 0;
+		else if (currentHealth >= maxHealth)
+			currentHealth = maxHealth;
 	}
 
 	public void kill() {
-		// TODO: coming soon
+		currentHealth = 0;
 	}
 
 	public void changePower(int value) {
-		// TODO: coming soon
-
+		powerValue += value;
+		if (powerValue >= powerMaxValue)
+			powerValue = powerMaxValue;
+		else if (powerValue <= 0)
+			powerValue = 0;
 	}
 
 	private void loadAnimations() {
-		// TODO: coming soon
+		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+		animations = new BufferedImage[7][8];
+		for (int j = 0; j < animations.length; j++)
+			for (int i = 0; i < animations[j].length; i++)
+				animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
 
+		statusBarImg = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
 	}
 
 	public void loadLvlData(int[][] lvlData) {
-		// TODO: coming soon
-
+		this.lvlData = lvlData;
+		if (!IsEntityOnFloor(hitbox, lvlData))
+			inAir = true;
 	}
 
 	public void resetDirBooleans() {
-		// TODO: coming soon
-
+		left = false;
+		right = false;
 	}
 
 	public void setAttacking(boolean attacking) {
-		// TODO: coming soon
+		this.attacking = attacking;
 	}
 
 	public boolean isLeft() {
-		// TODO: coming soon
-		return false;
+		return left;
 	}
 
 	public void setLeft(boolean left) {
-		// TODO: coming soon
+		this.left = left;
 	}
 
 	public boolean isRight() {
-		// TODO: coming soon
-		return false;
+		return right;
 	}
 
 	public void setRight(boolean right) {
-		// TODO: coming soon
+		this.right = right;
 	}
 
 	public void setJump(boolean jump) {
-		// TODO: coming soon
+		this.jump = jump;
 	}
 
 	public void resetAll() {
-		// TODO: coming soon
+		resetDirBooleans();
+		inAir = false;
+		attacking = false;
+		moving = false;
+		airSpeed = 0f;
+		state = IDLE;
+		currentHealth = maxHealth;
 
+		hitbox.x = x;
+		hitbox.y = y;
+		resetAttackBox();
+
+		if (!IsEntityOnFloor(hitbox, lvlData))
+			inAir = true;
 	}
 
 	private void resetAttackBox() {
-		// TODO: coming soon
-
+		if (flipW == 1) {
+			attackBox.x = hitbox.x + hitbox.width + (int) (Game.SCALE * 10);
+		} else {
+			attackBox.x = hitbox.x - hitbox.width - (int) (Game.SCALE * 10);
+		}
 	}
 
 	public int getTileY() {
-		// TODO: coming soon
-		return 0;
+		return tileY;
 	}
 
 	public void powerAttack() {
-		// TODO: coming soon
-
+		if (powerAttackActive)
+			return;
+		if (powerValue >= 60) {
+			powerAttackActive = true;
+			changePower(-60);
+		}
 
 	}
 
