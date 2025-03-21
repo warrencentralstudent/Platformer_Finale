5074a250175d0646a978f12fc96b2bf805fe6c70
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-21 05:32:34 -0400

    Enemy and Entity

--- src/entities/Crabby.java
+++ src/entities/Crabby.java
@@ -16,39 +16,81 @@ public class Crabby extends Enemy {
 
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
-        // TODO: coming soon
-
+        // TODO: call initHitbox() passing in 22, and 19
+        // TODO: call initAttackBox()
     }
 
     private void initAttackBox() {
-        // TODO: coming soon
-
+        // TODO: assign a new Rectangle2D.Float() with arguments x, y, (int)(82 * Game.SCALE), (int)(19 * Game.SCALE) to attackBox
+        // TODO: assign (int) (Game.SCALE * 30) to attackBoxOffsetX
     }
 
     public void update(int[][] lvlData, Player player) {
-        // TODO: coming soon
-
+        // TODO: call updateBehavior() passing in lvlData and player
+        // TODO: call updateAnimationTick()
+        // TODO: call updateAttackBox()
     }
 
     private void updateAttackBox() {
-        // TODO: coming soon
-
+        // TODO: assign hitbox.x - attackBoxOffsetX to attackBox.x
+        // TODO: assign hitbox.y to attackBox.y
     }
 
     private void updateBehavior(int[][] lvlData, Player player) {
-        // TODO: coming soon
+        // TODO: check if firstUpdate
+        // if block begin
+        // TODO: call firstUpdateCheck() passing in lvlData
+        // end of if block
+        // TODO: check if inAir
+        // if block begin
+        // TODO: call updateInAir() passing in lvlData
+        // end of if block
+        // else block begin
+        switch (state) {
+            case IDLE:
+                newState(RUNNING);
+                break;
+            case RUNNING:
+                if (canSeePlayer(lvlData, player)) {
+                    turnTowardsPlayer(player);
+                    if (isPlayerCloseForAttack(player))
+                        newState(ATTACK);
+                }
 
+                move(lvlData);
+                break;
+            case ATTACK:
+                if (aniIndex == 0)
+                    attackChecked = false;
+                if (aniIndex == 3 && !attackChecked)
+                    checkPlayerHit(attackBox, player);
+                break;
+            case HIT:
+                break;
+        }
+        // else block end
     }
 
     public int flipX() {
-        // TODO: coming soon
-        return 0;
+        // TODO: check if walkDir is equal to RIGHT
+        // if block begin
+        // TODO: return width;
+        // end of if block
+        // else block begin
+        // TODO: return 0
+        // else block end
+        return 0; // TODO: temp value, remove when done.
 
     }
 
     public int flipW() {
-        // TODO: coming soon
-        return 0;
-
+        // TODO: check if walkDir is equal to RIGHT
+        // if block begin
+        // TODO: return -1;
+        // end of if block
+        // else block begin
+        // TODO: return 1
+        // else block end
+        return 0; // TODO: temp value, remove when done.
     }
 }
