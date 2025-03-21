4a0614bf3f5b21f11d962b6d8e5d1b419514507f
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-21 05:27:25 -0400

    Enemy and Entity

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -1,6 +1,5 @@
 package entities;
 
-
 import utilz.Constants;
 import utilz.Constants.Game;
 
@@ -14,90 +13,167 @@ import static utilz.Constants.GRAVITY;
 import static utilz.HelpMethods.*;
 
 public abstract class Enemy extends Entity {
-	protected int enemyType;
-	protected boolean firstUpdate = true;
-	protected int walkDir = LEFT;
-	protected int tileY;
-	protected float attackDistance = Game.TILES_SIZE;
-	protected boolean active = true;
-	protected boolean attackChecked;
+    protected int enemyType;
+    protected boolean firstUpdate = true;
+    protected int walkDir = LEFT;
+    protected int tileY;
+    protected float attackDistance = Game.TILES_SIZE;
+    protected boolean active = true;
+    protected boolean attackChecked;
 
-	public Enemy(float x, float y, int width, int height, int enemyType) {
-		super(x, y, width, height);
-		// TODO: coming soon
+    public Enemy(float x, float y, int width, int height, int enemyType) {
+        super(x, y, width, height);
+        // TODO: assign enemyType to this.enemyType
+        // TODO: assign to maxHealth the value from GetMaxHealth(enemyType)
+        // TODO: assign to currentHealth the value of maxHealth
+        // TODO: assign to walkSpeed the value of Game.SCALE * 0.35f;
 
-	}
+    }
 
-	protected void firstUpdateCheck(int[][] lvlData) {
-		// TODO: coming soon
+    protected void firstUpdateCheck(int[][] lvlData) {
+        // TODO: check if !EntityOnFloor() passing in hitbox and lvlData
+        // if block start
+        // TODO: set inAir to true
+        // end of if block
+        // TODO: set firstUpdate to false
+    }
 
-	}
+    protected void updateInAir(int[][] lvlData) {
+        // TODO: check if CanMoveHere() passing in hitbox.x, hitbox.y + airSpeed, hitbox.width, hitbox.height, and lvlData
+        // if block start
+        // TODO: add airSpeed to the value of hitbox.y
+        // TODO: add GRAVITY to the value of airSpeed
+        // if block end
+        // else block start
+        // TODO: set inAir to false
+        // TODO: set hitbox.y to GetEntityYPosUnderRoofOrAboveFloor() passing in hitbox and airSpeed
+        // TODO: set tileY to (int) (hitbox.y / Game.TILES_SIZE)
+        // end of else block
+    }
 
-	protected void updateInAir(int[][] lvlData) {
-		// TODO: coming soon
+    protected void move(int[][] lvlData) {
+        // TODO: make a float named xSpeed and set to 0
+        // TODO: check if walkDir is equal to LEFT
+        // if block start
+        // TODO: set xSpeed to -walkSpeed
+        // end of if block
+        // else block start
+        // TODO: set xSpeed to walkSpeed
+        // else block end
 
-	}
+        // TODO: check if CanMoveHere() passing in hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, and lvlData
+        // if block start
+        // TODO: check if IsFloor() passing in hitbox, xSpeed, and lvlData
+        // if block start
+        // TODO: add xSpeed to hitbox.x
+        // TODO: return
+        // end of if block
+        // end of if block
+        // TODO: call changeWalkDir()
+    }
 
-	protected void move(int[][] lvlData) {
-		// TODO: coming soon
+    protected void turnTowardsPlayer(Player player) {
+        // TODO: check if player.hitbox.x is greater than hitbox.x
+        // if block start
+        // TODO: set walkDir to RIGHT
+        // end of if block
+        // else block start
+        // TODO: set walkDir to LEFT
+        // end of else block
+    }
 
-	}
+    protected boolean canSeePlayer(int[][] lvlData, Player player) {
+        // TODO: make an int called playerTileY and set to (int) (player.getHitbox().y / Game.TILES_SIZE
+        // TODO: check if playerTileY is equal to tileY
+        // if block begin
+        // TODO: check if isPlayerInRange() passing in player
+        // if block begin
+        // TODO: check if IsSightClear() passing in lvlData, hitbox, player.hitbox, and tileY
+        // if block begin
+        // TODO: return true
+        // if block end
+        // if block end
+        // if block end
+        return false;
+    }
 
-	protected void turnTowardsPlayer(Player player) {
-		// TODO: coming soon
+    protected boolean isPlayerInRange(Player player) {
+        // TODO: make an int called absValue and set to (int) Math.abs(player.hitbox.x - hitbox.x)
+        // TODO: return whether then absValue is less than or equal to attackDistance * 5
+        return false; // TODO: temp value.  remove when done.
+    }
 
-	}
+    protected boolean isPlayerCloseForAttack(Player player) {
+        // TODO: make an int called absValue and set to (int) Math.abs(player.hitbox.x - hitbox.x)
+        // TODO: return whether absValue is less than or equal to attackDistance
+        return false; // TODO: temp value.  remove when done.
+    }
 
-	protected boolean canSeePlayer(int[][] lvlData, Player player) {
-		// TODO: coming soon
-		return false;
-	}
+    protected void newState(int enemyState) {
+        // TODO: set this.state to enemyState
+        // TODO: set aniTick to 0
+        // TODO: set aniIndex to 0
+    }
 
-	protected boolean isPlayerInRange(Player player) {
-		// TODO: coming soon
-		return false;
-	}
+    public void hurt(int amount) {
+        // TODO: subtract amount from currentHealth
+        // TODO: check if currentHealth is less than or equal to 0
+        // if block begin
+        // TODO: call newState() passing in DEAD
+        // if block end
+        // else block begin
+        // TODO: call newState() passing in HIT
+        // else block end
+    }
 
-	protected boolean isPlayerCloseForAttack(Player player) {
-		// TODO: coming soon
-		return false;
-	}
+    protected void checkPlayerHit(Rectangle2D.Float attackBox, Player player) {
+        // TODO: check if attackBox.intersects(player.hitbox)
+        // if block begin
+        // TODO: call player.changeHealth() passing in -GetEnemyDmg(enemyType)
+        // if block end
+        // TODO: set attackChecked to true
+    }
 
-	protected void newState(int enemyState) {
-		// TODO: coming soon
+    protected void updateAnimationTick() {
+        // TODO: add 1 to aniTick
+        // TODO: check if aniTick is greater than or equal to ANI_SPEED
+        // if block begin
+        // TODO: set aniTick to 0
+        // TODO: add 1 to aniIndex
+        // TODO: check if aniIndex is greater than or equal to GetSpriteAmount(enemyType, state)
+        // if block begin
+        // TODO: set aniIndex to 0
+        switch (state) {
+            case ATTACK, HIT -> state = IDLE;
+            case DEAD -> active = false;
+        }
+        // end of if block
+        // end of if block
+    }
 
-	}
+    protected void changeWalkDir() {
+        // TODO: check if walkDir is equal to LEFT
+        // if block begin
+        // TODO: set walkDir to RIGHT
+        // end of if block
+        // else block begin
+        // TODO: set walkDir to LEFT
+        // end of else block
+    }
 
-	public void hurt(int amount) {
-		// TODO: coming soon
-
-	}
-
-	protected void checkPlayerHit(Rectangle2D.Float attackBox, Player player) {
-		// TODO: coming soon
-
-
-	}
-
-	protected void updateAnimationTick() {
-		// TODO: coming soon
-
-	}
-
-	protected void changeWalkDir() {
-		// TODO: coming soon
-
-	}
-
-	public void resetEnemy() {
-		// TODO: coming soon
-
-	}
+    public void resetEnemy() {
+        // TODO: set hitbox.x to x
+        // TODO: set hitbox.y to y
+        // TODO: set firstUpdate to true
+        // TODO: set currentHealth to maxHealth
+        // TODO: call newState() passing in IDLE
+        // TODO: set active to true
+        // TODO: set airSpeed to 0
+    }
 
 
-	public boolean isActive() {
-		// TODO: coming soon
-		return false;
-	}
+    public boolean isActive() {
+        return active;
+    }
 
 }
