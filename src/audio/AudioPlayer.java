be2480c86c43853db24b5044c3c98d53acd8119e
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-04-10 13:09:06 -0400

    Hey look free code in ui

--- src/audio/AudioPlayer.java
+++ src/audio/AudioPlayer.java
@@ -1,9 +1,16 @@
 package audio;
 
-import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.BooleanControl;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class AudioPlayer {
 
@@ -26,68 +33,124 @@ public class AudioPlayer {
     private Random rand = new Random();
 
     public AudioPlayer() {
-        // TODO: coming soon
+        loadSongs();
+        loadEffects();
+        playSong(MENU_1);
     }
 
     private void loadSongs() {
-        // TODO: coming soon
+        String[] names = { "menu", "level1", "level2" };
+        songs = new Clip[names.length];
+        for (int i = 0; i < songs.length; i++)
+            songs[i] = getClip(names[i]);
     }
 
     private void loadEffects() {
-        // TODO: coming soon
+        String[] effectNames = { "die", "jump", "gameover", "lvlcompleted", "attack1", "attack2", "attack3" };
+        effects = new Clip[effectNames.length];
+        for (int i = 0; i < effects.length; i++)
+            effects[i] = getClip(effectNames[i]);
+
+        updateEffectsVolume();
 
     }
 
     private Clip getClip(String name) {
-        // TODO: coming soon
+        URL url = getClass().getResource("/audio/" + name + ".wav");
+        AudioInputStream audio;
+
+        try {
+            audio = AudioSystem.getAudioInputStream(url);
+            Clip c = AudioSystem.getClip();
+            c.open(audio);
+            return c;
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+
+            e.printStackTrace();
+        }
+
         return null;
 
     }
 
     public void setVolume(float volume) {
-        // TODO: coming soon
+        this.volume = volume;
+        updateSongVolume();
+        updateEffectsVolume();
     }
 
     public void stopSong() {
-        // TODO: coming soon
+        if (songs[currentSongId].isActive())
+            songs[currentSongId].stop();
     }
 
     public void setLevelSong(int lvlIndex) {
-        // TODO: coming soon
+        if (lvlIndex % 2 == 0)
+            playSong(LEVEL_1);
+        else
+            playSong(LEVEL_2);
     }
 
     public void lvlCompleted() {
-        // TODO: coming soon
+        stopSong();
+        playEffect(LVL_COMPLETED);
     }
 
     public void playAttackSound() {
-        // TODO: coming soon
+        int start = 4;
+        start += rand.nextInt(3);
+        playEffect(start);
     }
 
     public void playEffect(int effect) {
-        // TODO: coming soon
+        effects[effect].setMicrosecondPosition(0);
+        effects[effect].start();
     }
 
     public void playSong(int song) {
-        // TODO: coming soon
+        stopSong();
+
+        currentSongId = song;
+        updateSongVolume();
+        songs[currentSongId].setMicrosecondPosition(0);
+        songs[currentSongId].loop(Clip.LOOP_CONTINUOUSLY);
     }
 
     public void toggleSongMute() {
-        // TODO: coming soon
+        this.songMute = !songMute;
+        for (Clip c : songs) {
+            BooleanControl booleanControl = (BooleanControl) c.getControl(BooleanControl.Type.MUTE);
+            booleanControl.setValue(songMute);
+        }
     }
 
     public void toggleEffectMute() {
-        // TODO: coming soon
+        this.effectMute = !effectMute;
+        for (Clip c : effects) {
+            BooleanControl booleanControl = (BooleanControl) c.getControl(BooleanControl.Type.MUTE);
+            booleanControl.setValue(effectMute);
+        }
+        if (!effectMute)
+            playEffect(JUMP);
     }
 
     private void updateSongVolume() {
 
-        // TODO: coming soon
+        FloatControl gainControl = (FloatControl) songs[currentSongId].getControl(FloatControl.Type.MASTER_GAIN);
+        float range = gainControl.getMaximum() - gainControl.getMinimum();
+        float gain = (range * volume) + gainControl.getMinimum();
+        gainControl.setValue(gain);
 
     }
 
     private void updateEffectsVolume() {
-        // TODO: coming soon
+        for (Clip c : effects) {
+            FloatControl gainControl = (FloatControl) c.getControl(FloatControl.Type.MASTER_GAIN);
+            float range = gainControl.getMaximum() - gainControl.getMinimum();
+            float gain = (range * volume) + gainControl.getMinimum();
+            gainControl.setValue(gain);
+        }
     }
 
 }
